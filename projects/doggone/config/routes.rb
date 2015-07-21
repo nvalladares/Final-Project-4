b91e4b0ff0e3ebeea12a9d6732ceae4f3c203181
Nancy Valladares <nvalladares120@gmail.com>
2015-07-20 20:08:22 -0500

    added comments by users

--- projects/doggone/config/routes.rb
+++ projects/doggone/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :comments
   get 'static_pages/home'
 
   get 'static_pages/help'
@@ -44,8 +46,7 @@ Rails.application.routes.draw do
 
   # Example resource route with more complex sub-resources:
   #   resources :products do
-  #     resources :comments
-  #     resources :sales do
+  #     #     resources :sales do
   #       get 'recent', on: :collection
   #     end
   #   end
